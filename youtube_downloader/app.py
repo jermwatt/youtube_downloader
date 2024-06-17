d48a5a0365d4e90f15106a54b4821ca52104b05b
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2024-06-17 08:55:38 -0700

    compose file added

--- youtube_downloader/app.py
+++ youtube_downloader/app.py
@@ -2,12 +2,17 @@ import streamlit as st
 from youtube_downloader.streams import get_yt_streams
 from youtube_downloader.download import download_thumbnail, download_joint_stream, download_separate_streams_and_join
 import tempfile
+import uuid
 
 
 st.title("Youtube Downloader")
 st.markdown("instructions: paste a valid youtube url in the textbox and download ")
 
 # Initialization
+if "url" not in st.session_state:
+    st.session_state["url"] = ""
+if "url_stream_count" not in st.session_state:
+    st.session_state["url_stream_count"] = 0
 if "stream_button_pressed" not in st.session_state:
     st.session_state["stream_button_pressed"] = False
 if "yt" not in st.session_state:
@@ -37,6 +42,41 @@ if "audio_only_streams" not in st.session_state:
     st.session_state["audio_only_streams"] = None
 if "audio_only_choices" not in st.session_state:
     st.session_state["audio_only_choices"] = None
+    
+    
+def reset_session_state():
+    if "stream_button_pressed" in st.session_state:
+        st.session_state["stream_button_pressed"] = False
+    if "yt" in st.session_state:
+        st.session_state["yt"] = None
+    if "yt_title" in st.session_state:
+        st.session_state["yt_title"] = None
+    if "yt_thumbnail_url" in st.session_state:
+        st.session_state["yt_thumbnail_url"] = None
+
+    if "a_v_selection_index" in st.session_state:
+        st.session_state["a_v_selection_index"] = 1
+    if "audio_video_streams" in st.session_state:
+        st.session_state["audio_video_streams"] = None
+    if "audio_video_choices" in st.session_state:
+        st.session_state["audio_video_choices"] = None
+
+    if "v_selection_index" in st.session_state:
+        st.session_state["v_selection_index"] = 0
+    if "video_only_streams" in st.session_state:
+        st.session_state["video_streams"] = None
+    if "video_only_choices" in st.session_state:
+        st.session_state["video_only_choices"] = None
+
+    if "a_selection_index" in st.session_state:
+        st.session_state["a_selection_index"] = 0
+    if "audio_only_streams" in st.session_state:
+        st.session_state["audio_only_streams"] = None
+    if "audio_only_choices" in st.session_state:
+        st.session_state["audio_only_choices"] = None
+
+    if "panel" in st.session_state:
+        del st.session_state["panel"]
 
 
 base = st.container(border=True)
@@ -48,15 +88,26 @@ with base:
             placeholder="your youtube url goes here",
             value="https://www.youtube.com/shorts/R1P1FgTrcHo",
         )
+        if st.session_state["url_stream_count"] == 0:
+            st.session_state["url"] = url
+        else:
+            if st.session_state["url"] != url:
+                st.session_state["url_stream_count"] = 0
+                st.session_state["url"] = url
+                if "panel" in st.session_state:
+                    del st.session_state["panel"]
+        
         col2, col3, col4 = st.columns([3, 2, 3])
         with col2:
             check_button_val = st.button(
                 label="fetch available streams", type="primary"
             )
         with col3:
-            empty_container = st.container()
+            panel = st.container()
         with col4:
-            placeholder = st.empty()
+            empty = st.empty()
+
+my_panel = st.empty()
 
 
 def get_set_streams(url: str) -> None:
@@ -97,7 +148,7 @@ def get_set_streams(url: str) -> None:
         st.session_state["audio_only_streams"] = audio_only_streams
 
 
-def download_button_logic(download_button_val: bool):
+def download_button_logic(download_button_val: bool):    
     if download_button_val:
         if st.session_state["a_v_selection_index"] == 0:
             if (
@@ -130,11 +181,14 @@ def download_button_logic(download_button_val: bool):
                         # download audio/video jointly
                         audio_index = st.session_state["a_selection_index"]
                         audio_only_streams = st.session_state["audio_only_streams"]
+                        audio_index -= 1
                         audio_selection = audio_only_streams[audio_index]
                         audio_itag = audio_selection.itag
                         
                         video_index = st.session_state["v_selection_index"]
+                        video_index -= 1
                         video_only_streams = st.session_state["video_only_streams"]
+
                         video_selection = video_only_streams[video_index]
                         video_itag = video_selection.itag
                         video_savepath = download_separate_streams_and_join(st.session_state["yt"],
@@ -173,6 +227,7 @@ def download_button_logic(download_button_val: bool):
 
                         # download audio/video jointly
                         index = st.session_state["a_v_selection_index"]
+                        index -= 1
                         audio_video_streams = st.session_state["audio_video_streams"]
                         selection = audio_video_streams[index]
                         itag = selection.itag
@@ -190,7 +245,8 @@ def download_button_logic(download_button_val: bool):
 
 
 def render_panel():
-    with st.container(border=True):
+    my_panel = st.empty()
+    with my_panel.container(border=True):
         col_a, col_b, col_c = st.columns([5, 5, 5])
         a_selection, v_selection, a_v_selection = None, None, None
 
@@ -236,20 +292,29 @@ def render_panel():
             else:
                 st.session_state["a_selection_index"] = 0
 
-    # download button
-    download_button_val = st.button(label="download selected stream", type="primary")
-    download_button_logic(download_button_val)
+        # download button
+        download_button_val = st.button(label="download selected stream", type="primary")
+        download_button_logic(download_button_val)
+    st.session_state["panel"] = my_panel
 
 
 def streams_button_logic(url: str) -> None:
     if check_button_val:
-        st.session_state["stream_button_pressed"] = True
-        get_set_streams(url)
-        render_panel()
+        if st.session_state["url_stream_count"] == 0:
+            st.session_state["url_stream_count"] += 1
+            reset_session_state()
+            st.session_state["stream_button_pressed"] = True
+            get_set_streams(url)
+            render_panel()
 
 
 if st.session_state["stream_button_pressed"]:
     render_panel()
 
 with st.spinner(text="streams pull in progress..."):
-    streams_button_logic(url)
+    try:
+        streams_button_logic(st.session_state["url"])
+    except:
+        pass
+
+            
